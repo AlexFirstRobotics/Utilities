d5b7b12506ed490f2ad636b267932ec97fc88bb1
AlexFirstRobotics <77585516+AlexFirstRobotics@users.noreply.github.com>
2021-09-30 15:41:05 -0500

    Can use to go to past year code

--- setup.sh
+++ setup.sh
@@ -6,19 +6,25 @@ then
 	mkdir -p FtcRobotCode-`date +%Y`
 fi
 
+echo "Year:"
+read $YEAR
+
+echo "Version:"
+read $VERSION
+
 VERSIONTAG=7.0;
-cd FtcRobotCode-`date +%Y`
-curl -L https://github.com/FIRST-Tech-Challenge/FtcRobotController/archive/v$VERSIONTAG.zip >FtcRobotController-$VERSIONTAG.zip
+cd FtcRobotCode-$YEAR
+curl -L https://github.com/FIRST-Tech-Challenge/FtcRobotController/archive/v$VERSION.zip >FtcRobotController-$VERSION.zip
 unzip FtcRobotController-$VERSIONTAG
 
 CURRHOMEPATH=`pwd`;
 for i in A B C; do
-	cp -r FtcRobotController-$VERSIONTAG FtcRobotController-$VERSIONTAG-$i
+	cp -r FtcRobotController-$VERSIONTAG FtcRobotController-$VERSION-$i
 	cd FtcRobotController-$VERSIONTAG-$i/TeamCode/src/main/java/org/firstinspires/ftc/
 	rm -rf teamcode
-	git clone https://github.com/ftcwaylandmi/`date +%Y`-$i.git teamcode
+	git clone https://github.com/ftcwaylandmi/$YEAR-$i.git teamcode
 	cd $CURRHOMEPATH;
 done
 
-rm -rf FtcRobotController-$VERSIONTAG
-rm -rf FtcRobotController-$VERSIONTAG.zip
+rm -rf FtcRobotController-$VERSION
+rm -rf FtcRobotController-$VERSION.zip
